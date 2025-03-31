dfffc1946c4f19bbc784e32efb6decabf036332e
RosieKhurmi <rosie.khurmi@ontariotechu.net>
2025-03-30 23:39:51 -0400

    Fix: Pass workflow

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import "./Navbar.css";
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect} from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
