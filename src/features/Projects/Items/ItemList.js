e5fe7d1984c58a1dec01cd4e49c948650ac8b1af
RosieKhurmi <khurmirosie@gmail.com>
2026-07-26 22:55:58 -0400

    update projects section

--- src/features/Projects/Items/ItemList.js
+++ src/features/Projects/Items/ItemList.js
@@ -1,54 +1,19 @@
 import React from 'react';
 import "./ItemList.css";
-import {useState, useEffect} from 'react'; 
-import {projectsData, projectsNav} from "./ProjectData";
+import {projectsData} from "./ProjectData";
 import Item from "./Item";
 
 const ItemList = () => {
-  const [item, setItem] = useState({name: 'All'});
-  const [projects, setProjects] = useState([]);
-  const [active, setActive] = useState (0);
-
-  useEffect(() => {
-    if (item.name === 'All') {
-      setProjects(projectsData);
-    } else {
-      const newProjects = projectsData.filter((project) => {
-        return project.type === item.name;
-      }); 
-      setProjects(newProjects);
-    }
-  }, [item]);
-
-  const handleClick = (e, index) => {
-    setItem({name: e.target.textContent}); 
-    setActive(index); 
-  }
-
   return (
     <div>
-      <div className = "works">
-
-        {projectsNav.map((item, index) => {
-            return (
-              <span onClick = {(e) => {
-                handleClick(e, index); 
-              }} className = {`${active === index ? 'active_cat' : ''} cat `}  key = {index}>
-                {item.name}
-              </span>
-            )
-        })}
-
-      </div>
-
       <div className = "personal_projects">
 
-        {projects.map((item) => {
+        {projectsData.map((item) => {
             return <Item item = {item} key = {item.id}/>
         })}
 
       </div>
-      
+
     </div>
   )
 }
--- src/features/Projects/Items/ProjectData.js
+++ src/features/Projects/Items/ProjectData.js
@@ -1,6 +1,6 @@
 import SMSImg from '../../../assets/Code/SMS.png';
 import PantryPalImg from '../../../assets/Code/PantryPal.png';
-import HackHive from '../../../assets/Code/HackHive.jpg';
+import AppTracker from '../../../assets/Code/AppTracker.png';
 import SocialSim from '../../../assets/Code/SocialSim.png';
 import StackSketch from '../../../assets/Code/StackSketch.png';
 
@@ -8,23 +8,20 @@ export const projectsData = [
     {
         id: 'SMS Spam Detector',
         code: "https://github.com/RosieKhurmi/SMSDetector.git",
-        type: 'AI/ML',
         img: SMSImg, 
         tech: ['Python', 'Scikit-learn', 'NLTK'],
         text: 'An SMS spam detector that uses natural language processing (NLP) techniques to classify messages as spam or ham. The detector uses a machine learning model trained on a dataset of SMS messages to predict whether a message is spam or ham.'
     }, 
     {
-        id: 'HackHive 2025 Website',
+        id: 'AppTracker',
         code: "https://github.com/RosieKhurmi/HackHive",
-        type: 'Extracurriculars',
-        img: HackHive,
-        tech: ['Angular', 'TypeScript'],
-        text: "Built the HackHive 2025 website with Angular & TypeScript, providing intuitive navigation and accessibility for a seamless hackathon experience."
+        img: AppTracker,
+        tech: ['JavaScript', 'Chrome Extension', 'Google Apps Script', 'REST API'],
+        text: "A Chrome extension that automatically captures job applications and logs them directly to a Google Sheet via Apps Script."
     }, 
     {
         id: "PantryPal - HackHive 2024 Winner",
         code: "https://github.com/smutharasan/PantryPal.git",
-        type: 'Software',
         img: PantryPalImg,
         tech: ['React', 'Azure', 'OpenAI', 'Spoonacular API'],
         text: "PantryPal is an AI-powered web app that generates recipes from an ingredients list using React, Azure, OpenAI, and Spoonacular API.",
@@ -32,7 +29,6 @@ export const projectsData = [
     {
         id: "StackSketch - SpurHacks 2025",
         code: "https://github.com/NightingaleX03/SpurHacks",
-        type: 'AI/ML',
         img: StackSketch,
         tech: ['FastAPI', 'Angular', 'Gemini', 'PyTorch', 'Hugging Face'],
         text: "A tool that uses AI to generate software system diagrams based on user prompts",
@@ -40,26 +36,9 @@ export const projectsData = [
     {
         id: "SocialSim - Hack the 6ix 2025",
         code: "https://github.com/hack-the-6ix-2025/SocialSim",
-        type: 'AI/ML',
         img: SocialSim,
         tech: ['TwelveLabs summarizer/embedding', 'Gemini API', 'Google Cloud', 'SciKit'],
         text: "AI Social Skills Training Platform for Healthcare, Law Enforcement, Education & Social Work Professionals",
     }
-];
-
-export const projectsNav = [
-
-    {
-        name: 'All'
-    },
-    {
-        name: 'Software',
-    },
-    {
-        name: 'AI/ML',
-    },
-    {
-        name: 'Extracurriculars',
-    },
 ];
 
